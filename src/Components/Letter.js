684b6dba851c4636f5f060d8e10d2e76c3286a17
Kristine Hilary MIlan <khilarymilan@Kristines-MacBook-Air.local>
2017-11-22 13:56:01 +0800

    refactor letter component

--- src/Components/Letter.js
+++ src/Components/Letter.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Letter = (props) => {
-
   const LetterStyle = {
     letter: {
-      color: props.color
-    }
+      color: props.color,
+    },
   };
 
   return (
-    <div style={ LetterStyle.letter }>{ props.letter }</div>
+    <div style={LetterStyle.letter}>{props.letter}</div>
   );
-}
+};
+
+Letter.propTypes = {
+  color: PropTypes.string.isRequired,
+  letter: PropTypes.string.isRequired,
+};
 
 export default Letter;
